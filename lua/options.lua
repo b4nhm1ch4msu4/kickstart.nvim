a17799d87d2d040f9e780c1216d7e3d97a6e7be8
Laptop HP <tienpham102vp@gmail.com>
2025-10-06 10:56:53 +0700

    add mason bin to path

--- lua/options.lua
+++ lua/options.lua
@@ -36,3 +36,9 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.fn.has 'win32' ~= 0
+local sep = is_windows and '\\' or '/'
+local delim = is_windows and ';' or ':'
+vim.env.PATH = table.concat({ vim.fn.stdpath 'data', 'mason', 'bin' }, sep) .. delim .. vim.env.PATH
