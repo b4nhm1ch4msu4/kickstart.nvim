512de169ee9c5492e5790e32c1e87b08e6f741a2
tien2.pham <tien2.pham@lge.com>
2025-10-07 23:18:07 +0700

    add robot formater

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -14,7 +14,7 @@ return { -- Autoformat
     },
   },
   opts = {
-    notify_on_error = false,
+    notify_on_error = true,
     format_on_save = function(bufnr)
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
@@ -29,8 +29,21 @@ return { -- Autoformat
         }
       end
     end,
+    formatters = {
+      robocop = {
+        command = 'robocop',
+        -- A list of strings, or a function that returns a list of strings
+        -- Return a single string instead of a list to run the command in a shell
+        args = { 'format', '$FILENAME' },
+        stdin = false,
+        range_args = function(self, ctx)
+          return { 'format', '--start-line', ctx.range.start[1], '--end-line', ctx.range['end'][1], '$FILENAME' }
+        end,
+      },
+    },
     formatters_by_ft = {
       lua = { 'stylua' },
+      robot = { 'robocop' },
     },
   },
 }
